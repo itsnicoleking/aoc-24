fbe1145625be2afc8570a3afab45ffc12fb8e532
Nicole King <19323801+itsnicoleking@users.noreply.github.com>
2024-12-24 13:03:40 +1000

    Day 6 pt 2 WIP

--- day6/day6.py
+++ day6/day6.py
@@ -3,6 +3,94 @@ from typing import TextIO
 
 gm = None
 
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def __str__(self):
+        return str(self.data)
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def __str__(self):
+        totalStr = ""
+
+        current = self.head
+        while current != None:
+            totalStr += f"{str(current)}, "
+            current = current.next
+
+        return totalStr
+
+    def insertAtStart(self, data):
+        newNode = Node(data)
+        if self.head == None:
+            self.head = newNode
+            return
+
+        newNode.next = self.head
+        self.head = newNode
+
+    def insertAtEnd(self, data):
+        newNode = Node(data)
+        if self.head == None:
+            self.head = newNode
+            return
+
+        current = self.head
+        while current.next:
+            current = current.next
+
+        current.next = newNode
+
+    def insert(self, index: int, data):
+        if index == 0:
+            self.insertAtStart(data)
+            return
+
+        pos = 0
+        current = self.head
+        while current != None and pos + 1 != index:
+            pos += 1
+            current = current.next
+
+        if current != None:
+            newNode = Node(data)
+            newNode.next = current.next
+            current.next = newNode
+
+    def existsWithNext(self, data, nextData) -> bool:
+        if self.head == None:
+            return False
+
+        current = self.head
+        while current != None and current.next != None:
+            if current.data == data and current.next.data == nextData:
+                return True
+            current = current.next
+
+        return False
+
+    def isCyclic(self) -> bool:
+        if self.head == None:
+            return False
+
+        slow = self.head
+        fast = self.head.next
+
+        while fast != None and fast.next != None:
+            if slow == fast:
+                return True
+
+            slow = slow.next
+
+            fast = fast.next.next
+
+        return False
+
 class GuardMap:
     map = []
     rows = 0
@@ -48,25 +136,61 @@ class GuardMap:
     def numDistinctPositions(self) -> int:
         positions = set()
 
-        while self.isValidCoordinates(self.posX, self.posY):
+        posX = self.posX
+        posY = self.posY
+        dirIdx = self.dirIdx
+
+        while self.isValidCoordinates(posX, posY):
             # Current position is visited and valid
-            positions.add((self.posX, self.posY))
+            positions.add((posX, posY))
 
             # Convenience
-            dirX = self.directions[self.dirIdx][0]
-            dirY = self.directions[self.dirIdx][1]
+            dirX = self.directions[dirIdx][0]
+            dirY = self.directions[dirIdx][1]
 
             # Provided the next step is in the map
-            if self.isValidCoordinates(self.posX + dirX, self.posY + dirY):
+            if self.isValidCoordinates(posX + dirX, posY + dirY):
                 # If is has a blockage, turn 90
-                if self.map[self.posY + dirY][self.posX + dirX] == '#':
-                    self.dirIdx = (self.dirIdx + 1) % 4
+                if self.map[posY + dirY][posX + dirX] == '#':
+                    dirIdx = (dirIdx + 1) % 4
 
             # Update position for next round
-            self.posX += self.directions[self.dirIdx][0]
-            self.posY += self.directions[self.dirIdx][1]
+            posX += self.directions[dirIdx][0]
+            posY += self.directions[dirIdx][1]
 
         return len(positions)
+
+    def doesMapLoop(self, blockageX: int, blockageY: int) -> bool:
+        positions = LinkedList()
+
+        posX = self.posX
+        posY = self.posY
+        dirIdx = self.dirIdx
+
+        while self.isValidCoordinates(posX, posY):
+            # Convenience
+            dirX = self.directions[dirIdx][0]
+            dirY = self.directions[dirIdx][1]
+
+            # Provided the next step is in the map
+            if self.isValidCoordinates(posX + dirX, posY + dirY):
+                # If is has a blockage, turn 90
+                if self.map[posY + dirY][posX + dirX] == '#' or \
+                    (posX + dirX == blockageX and posY + dirY == blockageY):
+                    dirIdx = (dirIdx + 1) % 4
+
+            # Add current location to saved
+            # TODO: only insert if that Node doesn't exist in the list
+            if positions.existsWithNext((posX, posY), (posX + dirX, posY + dirY)):
+                return True
+            else:
+                positions.insertAtEnd((posX, posY))
+
+            # Update position for next round
+            posX += self.directions[dirIdx][0]
+            posY += self.directions[dirIdx][1]
+
+        return False
 
 def setup():
     global gm
@@ -78,8 +202,23 @@ def setup():
 def part1() -> int:
     return gm.numDistinctPositions()
 
+def part2() -> int:
+    numMapsWithLoop = 0
+
+    for i in range(gm.rows):
+        for j in range(gm.cols):
+            print(j, i)
+            if gm.doesMapLoop(j, i):
+                numMapsWithLoop += 1
+
+    # print()
+    # print(gm.doesMapLoop(3, 6))
+
+    return numMapsWithLoop
+
 if __name__ == "__main__":
     setup()
-
     print("Part 1:\t", part1())
-    # print("Part 2:\t", part2())
+
+    setup()
+    print("Part 2:\t", part2())
